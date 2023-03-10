cf94cb203234ff8554f205055066790364e585b5
Rachana_2003 <rachanajaiswar18@gmail.com>
2023-03-10 15:48:37 +0530

    Added pattern program

---
+++ Pattern_1.java
@@ -0,0 +1,16 @@
+class Pattern_1
+{
+	public static void main(String args[])
+	{
+		System.out.println("Here's the pattern program ");
+		for(int i=1;i<=5;i++)
+		{
+			for(int j=1;j<=i;j++)
+			{
+				System.out.print("*");
+			}
+			System.out.println("");
+		}
+	}
+}
+ 
